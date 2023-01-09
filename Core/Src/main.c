7657a21ccdac24d1d7ffa1610f2b5ed00664dccc
ali abdollahian <aliansgp@gmail.com>
2023-01-09 21:20:47 +0330

    Add libraries to pass the warnings

--- Core/Src/main.c
+++ Core/Src/main.c
@@ -27,7 +27,8 @@
 
 /* Private includes ----------------------------------------------------------*/
 /* USER CODE BEGIN Includes */
-
+#include "LCD_LIB.h"
+#include "stdio.h"
 /* USER CODE END Includes */
 
 /* Private typedef -----------------------------------------------------------*/
