e9f92ceb531b84f39fafad8f8f0b0504185fc502
ali abdollahian <aliansgp@gmail.com>
2023-01-09 21:44:19 +0330

    Revert "Edit a bug in LCD_delay function"
    This reverts commit b5202a48391f8abb83b865dea1a28dcb23ba2e1f.

--- Debug/Core/Src/main.d
+++ Debug/Core/Src/main.d
@@ -34,7 +34,7 @@ Core/Src/main.o: ../Core/Src/main.c ../Core/Inc/main.h \
  ../Drivers/STM32F4xx_HAL_Driver/Inc/stm32f4xx_hal_hcd.h \
  ../Drivers/STM32F4xx_HAL_Driver/Inc/stm32f4xx_ll_usb.h ../Core/Inc/i2c.h \
  ../Core/Inc/main.h ../Core/Inc/i2s.h ../Core/Inc/spi.h ../Core/Inc/tim.h \
- ../USB_HOST/App/usb_host.h ../Core/Inc/gpio.h ../Core/Inc/LCD_LIB.h
+ ../USB_HOST/App/usb_host.h ../Core/Inc/gpio.h
 ../Core/Inc/main.h:
 ../Drivers/STM32F4xx_HAL_Driver/Inc/stm32f4xx_hal.h:
 ../Core/Inc/stm32f4xx_hal_conf.h:
@@ -77,4 +77,3 @@ Core/Src/main.o: ../Core/Src/main.c ../Core/Inc/main.h \
 ../Core/Inc/tim.h:
 ../USB_HOST/App/usb_host.h:
 ../Core/Inc/gpio.h:
-../Core/Inc/LCD_LIB.h:
